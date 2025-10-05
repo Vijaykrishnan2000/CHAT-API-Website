b06a1436f374985648b0993a174d1bf54e92d966
Vijaykrishnan2000 <67972944+Vijaykrishnan2000@users.noreply.github.com>
2025-10-05 16:38:44 +0530

    Update salesforceProxy.js

--- netlify/functions/salesforceProxy.js
+++ netlify/functions/salesforceProxy.js
@@ -12,7 +12,7 @@ exports.handler = async (event) => {
   const client_id = "3MVG9dAEux2v1sLtg4lmb4zhS6Pm1WapDQ2j_fVl4AGyt1o6Xr8hU_Z7bnZLUP.FKwjGRMl4FnymNFcKdpxnE";
   const client_secret = "85D3C163EBCE3EDA1A43B34FBDAD11082CE76414DB656B1FF177C0BF17FE7CD1";
   const username = "vijaykrishnan@agentforce.com";
-  const password = "NarutoTest@1374900719j6WpaR4zmSUQb0tuBF9ceI";
+  const password = "NarutoTest@1374900819j6WpaR4zmSUQb0tuBF9ceI";
 
   try {
     const tokenRes = await fetch("https://login.salesforce.com/services/oauth2/token", {
